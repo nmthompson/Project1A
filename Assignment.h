3c8e83ac207015270f41a275845486e4500dc812
Emily Thomas <ertgd8@mail.umkc.edu>
2015-02-20 18:47:45 -0600

    Update Assignment.h
    Added "getAssignedDate" in class Assignment for sorting list of assignments by assigned date

--- Assignment.h
+++ Assignment.h
@@ -9,7 +9,6 @@ struct Node
 	Node* next;
 	Node* prev;
 	Assignment Data;
-	
 };
 
 class Assignment
@@ -18,13 +17,13 @@ public:
 	Assignment(string desc, Date due, Date assigned, status s = Assigned);
 	string getDesc(){ return desc; }
 	Date getDueDate(){ return due; }
+	Date getAssignedDate() { return assigned; }
 	status getStatus(){ return s; }
 	void setDesc(string descr){ desc = descr; }
 	void setDueDate(Date dueWhen){ due = dueWhen; }
 	void setStatus(status SetStatus){ s = SetStatus; }
 
 private:
-	
 	status s;
 	string desc;
 	Date due;
