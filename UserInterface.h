9e1242f79d639c76004bb78f0c928f887f5281ea
Nick Thompson <nmtd63@KC-ISSS-RA03.kc.umkc.edu>
2015-03-01 15:02:42 -0600

    test

--- UserInterface.h
+++ UserInterface.h
@@ -1,32 +1,34 @@
+#pragma once
 #include "Date.h"
+#include "StringTokenizer.h"
 #include "Assignment.h"
-#include <iostream>
+#include "AssignmentManager.h"
 #include <fstream>
-#ifndef USERINTERFACE_H
-#define USERINTERFACE_H
+#include <string>
+#include <list>
+#include <iostream>
 
 using namespace std;
 
 class UserInterface{
-	//I think my syntax might be off and my logic as a result for these getters and setters
+    //I think my syntax might be off and my logic as a result for these getters and setters
 public:
-	UserInterface(){}
-	//void setChoice(int user_choice){
-	//	choice = user_choice;
-	//}
-	//int getChoice(){
-	//	return choice;
-	//}
-	//void setRead(char yes_or_no){
-	//	y_or_n = yes_or_no;
-	//}
-	//char getRead(){
-	//	return y_or_n;
-	//}
-	void uiLoop();
-	void fileRead();
+    UserInterface(){}
+    //void setChoice(int user_choice){
+    //  choice = user_choice;
+    //}
+    //int getChoice(){
+    //  return choice;
+    //}
+    //void setRead(char yes_or_no){
+    //  y_or_n = yes_or_no;
+    //}
+    //char getRead(){
+    //  return y_or_n;
+    //}
+    void uiLoop();
+    void fileRead();
 private:
-	string file_read_in;
-	string file_read_out;
+    string file_read_in;
+    string file_read_out;
 };
-#endif
