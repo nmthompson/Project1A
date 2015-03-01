5feee80a9637f77bb209d38227dbae2b4a2a3429
Emily Thomas <ertgd8@mail.umkc.edu>
2015-03-01 13:38:42 -0600

    AssignmentManager.h
    Added an overloading << operator in this header file

--- AssignmentManager.h
+++ AssignmentManager.h
@@ -3,21 +3,33 @@
 #include "Date.h"
 #include <list>
 
-using namespace std;
+ostream& operator <<(ostream& out, Assignment& rhs) { // Stream out the display data of an Assignment
+	status stat = rhs.getStatus();
+	if ((!rhs.isCompleted())) { // If assignment is not completed, don't try to print out the (nonexistent) 
+			                          // completion date
+	    out << rhs.dateString(rhs.getDueDate()) << rhs.getDesc() << rhs.dateString(rhs.getAssignedDate()) 
+			<< strConvert(stat);
+		return out;
+	}
+    out << rhs.dateString(rhs.getDueDate()) << rhs.getDesc() << rhs.dateString(rhs.getAssignedDate()) 
+		<< rhs.dateString(rhs.getCompletedDate()) << strConvert(stat);
+	return out;
+}
 
 class AssignmentManager
 {
 public:
 	// Initializes the number of late assignments to 0 and changes to assignment lists to false
 	AssignmentManager() {late_count = 0, lists_change = false;}
-	Assignment createAssignment(string& assign_str); // Creates an assignments from a string
+	Assignment createAssignment(string& str); // Creates an assignments from a string
 
 	// Tries to add an assignment to a list
-	void add(Assignment assignment, list<Assignment>& the_list, list<Assignment>::iterator& iter); 
+	void add(Assignment& assignment, list<Assignment>& the_list, list<Assignment>::iterator& iter); 
 	void remove(Assignment& assignment, list<Assignment>& the_list); // Removes an assignment from a list
 	// Manage the display of a list of assignments
 	void displayList(list<Assignment>& the_list, list<Assignment>::iterator& iter, string type_assign);
-	void complete(list<Assignment>& the_list, list<Assignment>::iterator& iter); // Complete an assignment
+	// Complete an assignment
+	void complete(list<Assignment>& the_list, list<Assignment>& comp_list, list<Assignment>::iterator& iter); 
 	void edit(list<Assignment>& the_list, list<Assignment>::iterator& iter); // Edit an assignment
 
 	int& getLateCount() {return late_count;} // Gets the current total number of late assignments
